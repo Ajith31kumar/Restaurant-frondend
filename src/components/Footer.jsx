c75f267c12d5b48c267a710296dec45b0635cf20
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2024-03-19 10:34:10 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Ajith Kumar</div>
+          <div className="right">
+            <p>Tamil Nadu , Chennai</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CODEWITHAjith</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By CodeWithAjith.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
